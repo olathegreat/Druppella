e1ff308b7f45d4db0a5f093363b93de28ba0311d
olathegreat <ademolarotimi2000@gmail.com>
2022-08-31 22:02:03 +0100

    todolist

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Todo from './Todo';
 import {Routes, Route} from "react-router-dom";
-import SignInForm from './SignInForm';
-import SignUpForm from './SignUpForm';
-import "./Style.css";
-import Dashboard from './Dashboard';
-import SignInCard from './SignInCard';
-import UpdateProfile from './UpdateProfile';
-import ForgotPassword from './ForgotPassword';
+
+import "./Style.css"
+import SearcherApp from './SearcherApp';
+
 
 
 const App = () => {
+  
   return (
+
     <Routes>
-       <Route exact path='/' element={<SignInCard/>}/>
-       <Route path='/forgotpassword' element={<ForgotPassword/>}/>
-       <Route path='/update-profile' element={<UpdateProfile/>}/>
-       
+            <Route path='/' element={<Todo/>}/>
+            <Route path='/search' element={<SearcherApp/>}/>
+      
 
-       <Route path='/signinform' element={<SignInForm/>}/>
-       <Route path='/signupform' element={<SignUpForm/>}/>
-
-
-       <Route path="/dashboard" element={<Dashboard/>}/>
-    
-
-
-   </Routes> 
-    
-   
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
-import Overall from "./components/Overall";
-import App from "./components/App"
-import {AuthProvider} from "./components/Contexts/AuthContext"
+import App from "./components/App";
+
 
 
 
@@ -12,9 +11,9 @@ ReactDOM.render(
     
   <React.StrictMode>
   <BrowserRouter>
-  <AuthProvider>
+
      <App/>
-  </AuthProvider>
+
  
   </BrowserRouter>
 </React.StrictMode>,
